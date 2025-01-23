1e5437b09920daa06e1d1cf2c038aa5389e7cedb
jackrichter <jackrichter1@gmail.com>
2025-01-23 05:03:02 +0100

    Important correction to Api method createTaskDetail.

--- src/main/java/uk/oakacademy/taskservice/controllers/TaskDetailController.java
+++ src/main/java/uk/oakacademy/taskservice/controllers/TaskDetailController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.oakacademy.taskservice.dto.TaskDetailDTO;
@@ -17,7 +18,7 @@ public class TaskDetailController {
     private final TaskDetailService taskDetailService;
 
     @PostMapping
-    public ResponseEntity<TaskDetailDTO> addTaskDetail(TaskDetailDTO taskDetailDTO) {
-        return new ResponseEntity<>(taskDetailService.save(taskDetailDTO), HttpStatus.CREATED);
+    public ResponseEntity<TaskDetailDTO> addTaskDetail(@RequestBody TaskDetailDTO taskDetailDTO) {
+        return new ResponseEntity<TaskDetailDTO>(taskDetailService.save(taskDetailDTO), HttpStatus.CREATED);
     }
 }
