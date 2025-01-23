aa26b271d61fc0be9678ffb1a4fb2516ad9eb405
jackrichter <jackrichter1@gmail.com>
2025-01-23 04:51:23 +0100

    Adding custom Repository methods (ElasticSearch).

--- src/main/java/uk/oakacademy/taskservice/repositories/TaskDetailRepository.java
+++ src/main/java/uk/oakacademy/taskservice/repositories/TaskDetailRepository.java
@@ -5,6 +5,6 @@ import uk.oakacademy.taskservice.entities.TaskDetail;
 
 import java.util.List;
 public interface TaskDetailRepository extends ElasticsearchRepository<TaskDetail, String> {
-//    public List<TaskDetail> findByTaskDescriptionContains(String description);
-//    public  List<TaskDetail> findByTaskTitleContains(String title);
+    public List<TaskDetail> findByTaskDescriptionContains(String description);
+    public  List<TaskDetail> findByTaskTitleContains(String title);
 }
